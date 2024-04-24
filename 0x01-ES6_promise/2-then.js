e6898aba381da9688e0cae321dfd09d27ae4863f
robberbadeia <robber.badeia@gmail.com>
2024-04-24 09:55:00 +0200

    Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,8 @@
 function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
