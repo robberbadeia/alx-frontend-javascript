35359d9df9fc5622f5e6f999ccfacecaf09cc738
robberbadeia <robber.badeia@gmail.com>
2024-04-24 11:22:20 +0200

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Call the signUpUser and uploadPhoto functions
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-
-  // Create an array of promises
-  const promises = [promise1, promise2];
-
-  // Return a Promise that settles when all the provided Promises have either resolved or rejected
-  return Promise.allSettled(promises).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const result = [];
+  return Promise.allSettled(promises).then((results) => {
+    results.map(({ status, value, reason }) => result.push({
+      status,
+      value: status === 'rejected' ? reason.toString() : value,
+    }));
+    return result;
+  });
 }
