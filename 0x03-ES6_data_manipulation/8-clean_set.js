eca24842db5cc3487bb4301bfab0c6d57f9187ee
robberbadeia <robber.badeia@gmail.com>
2024-04-29 20:25:05 +0300

    Task 8: Adjust return statment

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 function cleanSet(set, startString) {
   if (!startString || !startString.length || typeof startString !== 'string') return '';
   const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  return filteredValues.join('-');
+  return filteredValues.map((item) => item.slice(startString.length)).join('-');
 }
 
 export default cleanSet;
