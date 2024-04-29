458be4c3ef951e32b9b1e360c3fc568fbb0ed8e1
robberbadeia <robber.badeia@gmail.com>
2024-04-29 20:20:53 +0300

    Task 8: code formating

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 function cleanSet(set, startString) {
   if (!startString || !startString.length || typeof startString !== 'string') return '';
   const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  return filteredValues.join(' - ');
+  return filteredValues.join('-');
 }
 
 export default cleanSet;
