50f691bb2547a75feb53f4eaca55e8a132e20139
robberbadeia <robber.badeia@gmail.com>
2024-04-30 19:48:56 +0300

    Task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -39,4 +39,40 @@ export const printTeacher: printTeacherFunction = (
   lastName: string
 ): string => `${firstName[0]}. ${lastName}`;
 
-console.log(printTeacher('john', 'doe'));
+console.log(printTeacher("john", "doe"));
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+}
+interface StudentClassConstructorInterface {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+function createStudent(
+  cStudent: StudentClassConstructorInterface,
+  firstName: string,
+  lastName: string
+): StudentClassInterface {
+  return new cStudent(firstName, lastName);
+}
+
+const student1 = createStudent(StudentClass, "mike", "tyson");
+console.log(student1);
