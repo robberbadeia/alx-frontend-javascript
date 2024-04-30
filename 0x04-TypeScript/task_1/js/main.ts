c3c04c5e10e8474238d34e9ed475e21439819a62
robberbadeia <robber.badeia@gmail.com>
2024-04-30 16:39:27 +0300

    Task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,36 @@ interface Teacher {
   [key: string]: any;
 }
 
+const teacher3: Teacher = {
+  firstName: "John",
+  fullTimeEmployee: false,
+  lastName: "Doe",
+  location: "London",
+  contract: false,
+};
+
+console.log(teacher3);
+
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => `${firstName[0]}. ${lastName}`;
+
+console.log(printTeacher('john', 'doe'));
